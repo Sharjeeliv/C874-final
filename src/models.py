87d230bb67445fcebcbd7675cc8d31374a8a3f30
Sharjeel Mustafa <sharjeeliv@gmail.com>
2025-07-21 21:08:06 -0400

    Stash

--- src/models.py
+++ src/models.py
@@ -3,8 +3,8 @@ from recbole.model.general_recommender import BPR, LightGCN, ItemKNN, NeuMF
 from recbole.model.context_aware_recommender import FM, DeepFM, WideDeep
 
 MODELS = {
-    'BPR': BPR,
-    # 'LightGCN': LightGCN,
+    # 'BPR': BPR,
+    'LightGCN': LightGCN,
     # 'ItemKNN': ItemKNN,
     # 'NeuMF': NeuMF,
     # 'FM': FM,
